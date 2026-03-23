3d4b8f81449df7dae2da53d83f1fbb2c06eab5ac
Eilith404 <minaya.sofia@gmail.com>
2026-03-23 10:52:35 +0100

    Add testing coverage configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTest.ts'],
+    environment: "jsdom",
+    setupFiles: ["./src/setupTest.ts"],
     globals: true,
+    coverage: {
+      exclude: [
+        "**/*.config.ts",
+        "**/*.config.js",
+        "**/*.types.ts",
+        "**/*.d.ts",
+        "**/types",
+      ],
+      thresholds: {
+        functions: 80,
+      },
+    },
   },
-})
+});
